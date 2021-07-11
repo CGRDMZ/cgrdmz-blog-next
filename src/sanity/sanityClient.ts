1c4e204344f094159295c0f59735bb9bdac40f0b
hasankoroglu <dirahsan.irdemez@ceng.deu.edu.tr>
2021-07-11 17:22:30 +0300

    fixed a visual bug in middle screens.

--- src/sanity/sanityClient.ts
+++ src/sanity/sanityClient.ts
@@ -4,7 +4,7 @@ import SanityClient from "@sanity/client";
 const sanityClient = new SanityClient({
   projectId: "9ti3zf0y",
   token: process.env.SANITY_TOKEN,
-  dataset: process.env.NODE_ENV == "production" ? "production" : "development",
+  dataset: process.env.SANITY_DATASET,
   useCdn: false,
 });
 
