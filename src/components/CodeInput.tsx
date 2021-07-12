7cb6ff08a24a8e0f8c94b97d17a067a2f1d2230c
hasankoroglu <dirahsan.irdemez@ceng.deu.edu.tr>
2021-07-12 21:54:10 +0300

    optimizing build size by limiting the language support and also using light version of highlighter.

--- src/components/CodeInput.tsx
+++ src/components/CodeInput.tsx
@@ -1,12 +1,25 @@
 import { CodeNode } from "../sanity/serializers";
-import SyntaxHighlighter from "react-syntax-highlighter";
+import { LightAsync as SyntaxHighlighter } from "react-syntax-highlighter";
+import typescript from "react-syntax-highlighter/dist/cjs/languages/hljs/typescript"
 import { darcula } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
+SyntaxHighlighter.registerLanguage("ts", typescript)
+
 interface Props {
-  node: CodeNode
+  node: CodeNode;
 }
 
-export default function CodeInput({node}: Props) {
+export default function CodeInput({ node }: Props) {
   const { language, code } = node;
-  return <SyntaxHighlighter className="md:w-3/4 mx-auto sm:rounded-lg" style={darcula} language={language} showLineNumbers={true} useInlineStyles>{code}</SyntaxHighlighter>
+  return (
+    <SyntaxHighlighter
+      className="md:w-3/4 mx-auto sm:rounded-lg"
+      style={darcula}
+      language={language}
+      showLineNumbers={true}
+      useInlineStyles
+    >
+      {code}
+    </SyntaxHighlighter>
+  );
 }
