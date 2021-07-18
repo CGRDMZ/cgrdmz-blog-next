b7325ce4aa71d13ddb9aaaf1c7086af0b8982129
hasankoroglu <dirahsan.irdemez@ceng.deu.edu.tr>
2021-07-18 18:58:05 +0300

    fix category margin

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -3,5 +3,5 @@ interface Props {
 }
 
 export default function Category({ name }: Props) {
-  return <div className="bg-gray-400 text-white px-2 py-1 mr-1 rounded-full text-xs font-bold">{name}</div>;
+  return <div className="bg-gray-400 text-white px-2 py-1 mr-1 my-1 rounded-full text-xs font-bold">{name}</div>;
 }
