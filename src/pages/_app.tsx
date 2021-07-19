a583a6e7a1b1807c3bf1082882511afbcf39d61e
hasankoroglu <dirahsan.irdemez@ceng.deu.edu.tr>
2021-07-19 14:37:03 +0300

    no need for manual pageview for ga as it works with history changes by default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,16 +8,6 @@ import * as gtag from "../utils/ga";
 import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
   return (
     <>
       <Head>
