a3e147ebf6ebdb9cb8f68d605fca86ea687f46f3
hasankoroglu <dirahsan.irdemez@ceng.deu.edu.tr>
2022-04-13 18:49:54 +0300

    link serializer and remove justify text.

--- src/sanity/serializers.tsx
+++ src/sanity/serializers.tsx
@@ -1,6 +1,7 @@
 import BlockRenderer from "../components/sanity-serializers/BlockRenderer";
 import CodeInput from "../components/sanity-serializers/CodeInput";
 import ImageRenderer from "../components/sanity-serializers/ImageRenderer";
+import LinkRenderer from "../components/sanity-serializers/LinkRenderer";
 import { ListItemRenderer, ListRenderer } from "../components/sanity-serializers/ListRenderer";
 
 export interface CodeNode {
@@ -18,6 +19,9 @@ const serializers = {
     block: BlockRenderer,
     image: ImageRenderer,
   },
+  marks: {
+    link: LinkRenderer
+  }
 };
 
 export default serializers;
